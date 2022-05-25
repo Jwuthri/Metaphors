c8dadfb8dc5ecd9bf010a8ff6c84bbe1d65b254b
julien <julien.wut@gmail.com>
2022-05-25 00:27:07 -0700

    update streamlit app typo

--- metaphors/applications/bionic_reading/server/streamlit.py
+++ metaphors/applications/bionic_reading/server/streamlit.py
@@ -1,20 +1,25 @@
 import streamlit as st
 
 from metaphors.applications.bionic_reading import BionicReading
-from metaphors.applications.bionic_reading.settings import StopWordsBehavior, OutputFormat, RareBehavior
+from metaphors.applications.bionic_reading.settings import StopWordsBehavior, RareBehavior
 
 
 class BionicReadingApp:
+    """The BionicReadingApp class is a Python class that represents a Bionic Reading application"""
+
     def __init__(self):
         pass
 
     @staticmethod
     def start():
+        """
+        It creates a function that takes in the text and returns the text with the bionic reading features.
+        """
         st.title("Bionic Reading")
-        fixation = st.slider("fixation streng", min_value=0.0, max_value=1.0, value=0.7)
-        saccades = st.slider("saccades streng", min_value=0.0, max_value=1.0, value=0.7)
-        opacity = st.slider("opacity streng", min_value=0.0, max_value=1.0, value=0.7)
-        stopwords = st.slider("stopwords streng", min_value=0.0, max_value=1.0, value=0.7)
+        fixation = st.slider("fixation strength", min_value=0.0, max_value=1.0, value=0.7)
+        saccades = st.slider("saccades strength", min_value=0.0, max_value=1.0, value=0.7)
+        opacity = st.slider("opacity strength", min_value=0.0, max_value=1.0, value=0.7)
+        stopwords = st.slider("stopwords strength", min_value=0.0, max_value=1.0, value=0.7)
         stopwords_behavior = st.selectbox(
             "stopwords_behavior", [behavior.value.lower() for behavior in StopWordsBehavior]
         )
